aa0b2c6b127e602f8dfbb8fc5740d1331a927558
Eduardo Garcia Venegas <edugarcia.venegas@gmail.com>
2023-11-20 00:39:19 -0300

    Ejercicio con el punto C terminado y validaciones confirmadas

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -16,10 +16,19 @@ def actualizarRuleta (ruleta,n):
     else :
         print (-1)
 
-
-"""def ruleta():
+def porcentajeRuleta (ruleta, n):
+    if n >=0 and n <= 36:
+        sumaRuleta = sum(ruleta)
+        if ruleta[n] > 0:
+            porcentaje = (ruleta[n] * 100) / sumaRuleta
+            print(f"El numero {n} ha sido sorteado un {porcentaje} % de veces")
+        else:
+            print(f"El numero {n} ha sido sorteado 0 % de veces")
+    else:
+        print("Numero ingresado es incorrecto")
     
-    status = statusRuleta(ruleta)"""
+
+
 
 ruleta = [0] * 37
 statusRuleta(ruleta)
@@ -27,9 +36,9 @@ active = True
 while active:
     validaAccion= True
     while validaAccion:
-        accion = int(input("Si desea ver los numeros que NO han salido sorteados presione 1\nSi desea actuializar un numero en la ruleta presione 2\nSi desea obtener el porcentaje de sorteo de un numero presione 3: "))
+        accion = int(input("Si desea ver los numeros que NO han salido sorteados presione 1\nSi desea actualizar un numero en la ruleta presione 2\nSi desea obtener el porcentaje de sorteo de un numero presione 3\nSi desea ver el estado de la ruta presione 4: "))
         print("================================")
-        if accion == 1 or accion == 2 or accion == 3:
+        if accion in [1,2,3,4]:
             validaAccion = False
         else:
             print("La opcion ingresada no es valida")
@@ -42,8 +51,19 @@ while active:
         actualizarRuleta(ruleta,n)
         statusRuleta(ruleta) 
         print("================================")
+    elif accion == 3:
+        n = int(input("Ingrese numero para calcular porcentaje de sorteo: "))
+        porcentajeRuleta(ruleta, n)
+        print("================================")
+    elif accion == 4:
+        statusRuleta(ruleta)
+        print("================================")
 
 
+
+
+
+        
 
 
 
